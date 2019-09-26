b45fed9176c19647b9d91bf0ce4940e539c72c6b
zhaokejin <210006540@qq.com>
2019-09-26 14:19:36 +0800

    Site updated: 2019-09-26 14:19:35

--- js/search.js
+++ js/search.js
@@ -1,101 +1,59 @@
-var searchFunc = function (path, searchId, contentId) {
-    var BTN = '<i id="local-search-close">x</i>';
+var searchFunc = function (e, l, n) {
+    var c = '<i id="local-search-close">x</i>';
     $.ajax({
-        url: path,
-        dataType: 'xml',
-        success: function (xmlResponse) {
-            var datas = $('entry', xmlResponse).map(function () {
-                return {
-                    title: $('title', this).text(),
-                    content: $('content', this).text(),
-                    url: $('url', this).text()
-                };
-            }).get();
-
-            var $input = document.getElementById(searchId);
-            var $resultContent = document.getElementById(contentId);
-
-            $input.addEventListener('input', function () {
-                var str = '<ul class="search-result-list">';
-                var keywords = this.value.trim().toLowerCase().split(/[\s\-]+/);
-                $resultContent.innerHTML = '';
-                if (this.value.trim().length <= 0) {
-                    return;
-                }
-                datas.forEach(function (data) {
-                    var isMatch = true;
-                    if (!data.title || data.title.trim() === '') {
-                        data.title = 'Untitled';
-                    }
-                    var dataTitle = data.title.trim().toLowerCase();
-                    var dataContent = data.content.trim().replace(/<[^>]+>/g, '').toLowerCase();
-                    var dataUrl = data.url;
-                    var indexTitle = -1;
-                    var indexContent = -1;
-                    var firstOccur = -1;
-                    if (dataContent !== '') {
-                        keywords.forEach(function (keyword, i) {
-                            indexTitle = dataTitle.indexOf(keyword);
-                            indexContent = dataContent.indexOf(keyword);
-
-                            if (indexTitle < 0 && indexContent < 0) {
-                                isMatch = false;
-                            } else {
-                                if (indexContent < 0) {
-                                    indexContent = 0;
-                                }
-                                if (i === 0) {
-                                    firstOccur = indexContent;
-                                }
-                            }
-                        });
-                    } else {
-                        isMatch = false;
-                    }
-                    if (isMatch) {
-                        str += '<li><a href="' + dataUrl + '" class="search-result-title">' + dataTitle + '</a>';
-                        var content = data.content.trim().replace(/<[^>]+>/g, '');
-                        if (firstOccur >= 0) {
-                            var start = firstOccur - 20;
-                            var end = firstOccur + 80;
-
-                            if (start < 0) {
-                                start = 0;
-                            }
-
-                            if (start === 0) {
-                                end = 100;
-                            }
-
-                            if (end > content.length) {
-                                end = content.length;
-                            }
-
-                            var matchContent = content.substr(start, end);
-
-                            keywords.forEach(function (keyword) {
-                                var regS = new RegExp(keyword, 'gi');
-                                matchContent = matchContent.replace(
-                                    regS, '<em class=\"search-keyword\">' + keyword + '</em>');
-                            });
-
-                            str += '<p class="search-result">' + matchContent + '...</p>';
-                        }
-                        str += '</li>';
-                    }
-                });
-                str += '</ul>';
-                if (str.indexOf('<li>') === -1) {
-                    return $resultContent.innerHTML = BTN + '<ul><span class="local-search-empty">没有找到内容，更换下搜索词试试吧~<span></ul>';
-                }
-                $resultContent.innerHTML = BTN + str;
+        url: e,
+        dataType: "json",
+        success: function (e) {
+            var t = e.posts;
+            t.map(function (e) {
+                return e.content = e.text, e.url = "/" + e.path, e
             });
+            var a = document.getElementById(l),
+                r = document.getElementById(n);
+            a.addEventListener("input", function () {
+                var f = '<ul class="search-result-list">',
+                    p = this.value.trim().toLowerCase().split(/[\s\-]+/);
+                if (r.innerHTML = "", !(this.value.trim().length <= 0)) {
+                    if (t.forEach(function (e) {
+                        var a = !0;
+                        e.title && "" !== e.title.trim() || (e.title = "Untitled");
+                        var r = e.title.trim().toLowerCase(),
+                            l = e.content.trim().replace(/<[^>]+>/g, "").toLowerCase(),
+                            t = e.url,
+                            n = -1,
+                            c = -1,
+                            s = -1;
+                        if ("" !== l ? p.forEach(function (e, t) {
+                            n = r.indexOf(e), c = l.indexOf(e), n < 0 && c < 0 ?
+                                a = !1 : (c < 0 && (c = 0), 0 === t && (s = c))
+                        }) : a = !1, a) {
+                            f += '<li><a href="' + t + '" class="search-result-title">' +
+                                r + "</a>";
+                            var i = e.content.trim().replace(/<[^>]+>/g, "");
+                            if (0 <= s) {
+                                var o = s - 20,
+                                    u = s + 80;
+                                o < 0 && (o = 0), 0 === o && (u = 100), u > i.length &&
+                                (u = i.length);
+                                var h = i.substr(o, u);
+                                p.forEach(function (e) {
+                                    var t = new RegExp(e, "gi");
+                                    h = h.replace(t,
+                                        '<em class="search-keyword">' + e +
+                                        "</em>")
+                                }), f += '<p class="search-result">' + h +
+                                    "...</p>"
+                            }
+                            f += "</li>"
+                        }
+                    }), -1 === (f += "</ul>").indexOf("<li>")) return r.innerHTML = c +
+                        '<ul><span class="local-search-empty">没有找到内容，更换下搜索词试试吧~<span></ul>';
+                    r.innerHTML = c + f
+                }
+            })
         }
-    });
+    })
 };
-$(document).on('click', '#local-search-close', function () {
-    $('#local-search-input').val('');
-    $('#local-search-result').html('');
-});
-
-searchFunc('/search.xml', 'local-search-input', 'local-search-result');
+$(document).on("click", "#local-search-close", function () {
+    $("#local-search-input").val(""), $("#local-search-result").html("")
+}), searchFunc("/content.json", "local-search-input", "local-search-result");
